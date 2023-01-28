0d744b592d2d52699f35bf3ef070ba87c0e1463b
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-28 18:58:34 +0200

    Update Solution.cpp

--- Problems/LeetCode/2. Add Two Numbers/Solution.cpp
+++ Problems/LeetCode/2. Add Two Numbers/Solution.cpp
@@ -31,23 +31,19 @@
 class Solution {
   public:
   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode dummy(0);
-    ListNode* curr = &dummy;
+    ListNode* dummyHead = new ListNode(0);
+    ListNode* curr = dummyHead;
     int carry = 0;
-
-    while (l1 || l2 || carry) {
-      if (l1 != nullptr) {
-        carry += l1->val;
-        l1 = l1->next;
-      }
-      if (l2 != nullptr) {
-        carry += l2->val;
-        l2 = l2->next;
-      }
-      curr->next = new ListNode(carry % 10);
-      carry /= 10;
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+      int x = l1 ? l1->val : 0;
+      int y = l2 ? l2->val : 0;
+      int sum = carry + x + y;
+      carry = sum / 10;
+      curr->next = new ListNode(sum % 10);
       curr = curr->next;
+      l1 = l1 ? l1->next : nullptr;
+      l2 = l2 ? l2->next : nullptr;
     }
-    return dummy.next;
+    return dummyHead->next;
   }
 };
